1c5fa0eca40af95568487b4bae99214e18e6d4cb
Jesper Uddström <jesper.uddstrom@skandia.se>
2025-10-31 17:25:40 +0100

    validating dates with ajv

--- dynamic-form-app/src/App.tsx
+++ dynamic-form-app/src/App.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
 import ProductConfigForm from './ProductConfigForm';
 import productSchema from './productSchemaAli.json'; // Import your schema
-import { convertJsonSchemaToZod } from 'zod-from-json-schema';
-import { z } from 'zod';
+import Ajv from 'ajv/dist/2020';
+import addFormats from 'ajv-formats';
 
-const zodSchema = convertJsonSchemaToZod(productSchema as JSONSchema);
+const ajv = new Ajv({ strict: false, allErrors: true, allowDate: true });
+addFormats(ajv);
 
-type Product = z.infer<typeof zodSchema>;
-
-const p: Product = {
-    test: 'abc',
+const product = {
+    payoutStartDate: '2026-01-01',
     payoutPeriod: 64,
 };
 
-const valRes = zodSchema.safeParse(p);
+const validate = ajv.compile(productSchema);
+const isValid = validate(product);
 
 console.log('JsonSchema', productSchema);
-console.log('ZodSchema', zodSchema.shape);
-console.log('p', p);
-console.log('valRes', valRes);
+console.log('p', product);
+console.log('isValid', isValid);
 
 const App = () => {
-    const handleFormSubmit = (formData: HTMLFormElement) => {
-        console.log('Form Data:', formData);
-    };
+    // const handleFormSubmit = (formData: HTMLFormElement) => {
+    //     console.log('Form Data:', formData);
+    // };
 
-    return (
-        <div className='container mt-5'>
-            <ProductConfigForm zSchema={zodSchema} jSchema={productSchema} onSubmit={handleFormSubmit} />
-        </div>
-    );
+    // return (
+    //     <div className='container mt-5'>
+    //         <ProductConfigForm zSchema={zodSchema} jSchema={productSchema} onSubmit={handleFormSubmit} />
+    //     </div>
+    // );
+
+    return <div className='container mt-5'>Hello Ajv World!</div>;
 };
 
 export default App;
